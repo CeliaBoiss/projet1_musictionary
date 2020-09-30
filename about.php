8a77ca230f4696d5af658a44aecb9aea3bbe7c51
LucyMerlier <lucymerlier@gmail.com>
2020-09-30 14:11:41 +0200

    created and filled about.php

---
+++ about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MusicTionary</title>
+</head>
+<body>
+    <?php include "header.php"?>
+
+    <h2>About Us</h2>
+
+    <article class = "member">
+        <p class = "description right">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, adipisci at nulla, laudantium omnis corrupti cum architecto nobis unde quae, fugiat illum repellat odio! Numquam, nobis dolorem. Labore, voluptate. Quo!
+        </p>
+        <img class = "profileImage left" src="https://via.placeholder.com/75" alt="">
+    </article>
+
+    <article class = "member">
+        <p class = "description left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, adipisci at nulla, laudantium omnis corrupti cum architecto nobis unde quae, fugiat illum repellat odio! Numquam, nobis dolorem. Labore, voluptate. Quo!
+        </p>
+        <img class = "profileImage right" src="https://via.placeholder.com/75" alt="">
+    </article>
+
+    <article class = "member">
+        <p class = "description right">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, adipisci at nulla, laudantium omnis corrupti cum architecto nobis unde quae, fugiat illum repellat odio! Numquam, nobis dolorem. Labore, voluptate. Quo!
+        </p>
+        <img class = "profileImage left" src="https://via.placeholder.com/75" alt="">
+    </article>
+
+    <article class = "member">
+        <p class = "description left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, adipisci at nulla, laudantium omnis corrupti cum architecto nobis unde quae, fugiat illum repellat odio! Numquam, nobis dolorem. Labore, voluptate. Quo!
+        </p>
+        <img class = "profileImage right" src="https://via.placeholder.com/75" alt="">
+    </article>
+
+    <?php include "footer.php"?>
+</body>
+</html>

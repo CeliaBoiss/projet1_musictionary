a4e1b0c1e6a83d067c15e6e7bd45fcce1d7924d3
CeliaBoiss <boisseau.celia@gmail.com>
2020-09-30 14:12:07 +0200

    Created the structure of the form page

---
+++ contactus.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MusicTionary</title>
+</head>
+<body>
+    <?php include "header.php"?>
+    <form>
+        <input type="text" placeholder="Your name">
+        <input type="email" placeholder="Your email">
+        <input type="text" placeholder="The subject of your message">
+        <textarea placeholder="Your message"></textarea>
+        <button>Send !</button>
+    </form>
+    <?php include "footer.php"?>
+</body>
+</html>

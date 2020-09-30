568ff86fc5962c31eca6fd2924368d3a162ec552
AshBll <achillebellay@gmail.com>
2020-09-30 13:46:51 +0200

    deletion of doctype and meta tags in header and footer

--- footer.php
+++ footer.php
@@ -1,27 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <footer>
-        <ul>
-            <li><a href="">Item</a></li>
-            <li><a href="">Item</a></li>
-            <li><a href="">Item</a></li>
-        </ul>
-        <ul>
-            <li><a href="">Item</a></li>
-            <li><a href="">Item</a></li>
-            <li><a href="">Item</a></li>
-        </ul>
-        <ul>
-            <li><a href="">Item</a></li>
-            <li><a href="">Item</a></li>
-            <li><a href="">Item</a></li>
-        </ul>
-    </footer>
-</body>
-</html>
+<footer>
+    <ul>
+        <li><a href="">Item</a></li>
+        <li><a href="">Item</a></li>
+        <li><a href="">Item</a></li>
+    </ul>
+    <ul>
+        <li><a href="">Item</a></li>
+        <li><a href="">Item</a></li>
+        <li><a href="">Item</a></li>
+    </ul>
+    <ul>
+        <li><a href="">Item</a></li>
+        <li><a href="">Item</a></li>
+        <li><a href="">Item</a></li>
+    </ul>
+</footer>
--- header.php
+++ header.php
@@ -1,25 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-    <h1>MusicTionary</h1>
-        <nav>
-            <ul>
-                <li><a>Item 1</a></li>
-                <li><a>Item 2</a></li>
-                <li><a>Item 3</a></li>
-            </ul>
-            <div>
-                <label for=""></label>
-                <input type="search" id="" name="">
-                <button>Search</button>
-            </div>
-        </nav>
-    </header>
-</body>
-</html>
+<header>
+<h1>MusicTionary</h1>
+    <nav>
+        <ul>
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+            <li><a>Item 3</a></li>
+        </ul>
+        <div>
+            <label for=""></label>
+            <input type="search" id="" name="">
+            <button>Search</button>
+        </div>
+    </nav>
+</header>

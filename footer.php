f4a971297fe7f68bbe12e444a2ec7cd7c1250f1c
LucyMerlier <lucymerlier@gmail.com>
2020-09-30 13:52:44 +0200

    moded header and footer

--- footer.php
+++ footer.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <footer>
+<footer>
         <ul>
             <li><a href="">Item</a></li>
             <li><a href="">Item</a></li>
@@ -23,5 +15,3 @@
             <li><a href="">Item</a></li>
         </ul>
     </footer>
-</body>
-</html>

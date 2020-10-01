c1a62de6b18b7fa5efdd1aede8f3eaa8530269e6
LucyMerlier <lucymerlier@gmail.com>
2020-10-01 10:05:56 +0200

    added link to mainStyle.css in index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "mainStyle.css">
     <title>MusicTionary</title>
 </head>
 <body>
@@ -10,19 +11,19 @@
     <section title="articles">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
         <div>
-            <h2> Thème</h2>
+            <h2>Thème</h2>
                 <article>
                     <img src="https://via.placeholder.com/75" alt="">
                     <img src="https://via.placeholder.com/75" alt="">
                     <img src="https://via.placeholder.com/75" alt="">
                 </article>
-             <h2> Thème</h2>
+             <h2>Thème</h2>
                 <article>
                     <img src="https://via.placeholder.com/75" alt="">
                     <img src="https://via.placeholder.com/75" alt="">
                     <img src="https://via.placeholder.com/75" alt="">
                 </article>
-            <h2> Thème</h2>
+            <h2>Thème</h2>
                 <article>
                     <img src="https://via.placeholder.com/75" alt="">
                     <img src="https://via.placeholder.com/75" alt="">

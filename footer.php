6df689c5810198c639ad3786f4a896d59d05ccbe
LucyMerlier <lucymerlier@gmail.com>
2020-10-01 11:12:17 +0200

    added ids and classes to footer

--- footer.php
+++ footer.php
@@ -1,12 +1,15 @@
 <footer>
-    <ul class = "footerList">
-        <li class = "footerLink"><a href="">Item</a></li>
-        <li class = "footerLink"><a href="">Item</a></li>
-        <li class = "footerLink"><a href="">Item</a></li>
-    </ul>
-    <ul class = "footerList">
-        <li class = "footerLink"><a href="">Item</a></li>
-        <li class = "footerLink"><a href="">Item</a></li>
-        <li class = "footerLink"><a href="">Item</a></li>
-    </ul>
+    <hr>
+    <div id = "footerFlexDiv">
+        <ul class = "footerList">
+            <li class = "footerLink"><a href="">Item</a></li>
+            <li class = "footerLink"><a href="">Item</a></li>
+            <li class = "footerLink"><a href="">Item</a></li>
+        </ul>
+        <ul class = "footerList">
+            <li class = "footerLink"><a href="">Item</a></li>
+            <li class = "footerLink"><a href="">Item</a></li>
+            <li class = "footerLink"><a href="">Item</a></li>
+        </ul>
+    </div>
 </footer>

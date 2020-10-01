db2b0e460f4eddd2b6a8ca03f42fc183982c7280
Yann Raulet <yann.raulet@hotmail.fr>
2020-10-01 10:07:48 +0200

    Add sections Discover more to templateGenre

--- templateGenre.php
+++ templateGenre.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MusicTionary</title>
+
+    <link rel="stylesheet" type="text/css" href="templateGenreStyle.css"/>
+
+
+
 </head>
 <body>
     <?php include "header.php"?>
@@ -19,23 +24,45 @@
     </section>
 
     <section title = 'Few exemples'>
-        <h2>Quelques exemples</h2>
+        <h3>Quelques exemples</h3>
             <ul>
                 <li>Lorem ipsum dolor sit amet, consec</li>
                 <li>Lorem ipsum dolor sit amet, consec</li>
                 <li>Lorem ipsum dolor sit amet, consec</li>
             </ul>
+
+            <iframe width="350" height="200s" src="https://www.youtube.com/embed/videoseries?list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </section>
 
-    <section title ='video'>
 
-        <iframe width="350" height="200s" src="https://www.youtube.com/embed/videoseries?list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    
+	<section>
+        <h2>Discover More</h2>
+		<div class="DiscoverFirstBlock">
+            <img src="https://via.placeholder.com/75" alt="">
+			<aside>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+			</aside>	
+        </div>
     </section>
 
     <section>
-            <h2>Discover More</h2>
+		<div class="DiscoverSecondBlock">
+			<article>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+			</article>
+            <img src="https://via.placeholder.com/75" alt="">
+		</div>
+	</section>
+
+    <section>
+		<div class="DiscoverThirdBlock">
+            <img src="https://via.placeholder.com/75" alt="">
+			<article>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+			</article>	
+        </div>
     </section>
+
 
 
 

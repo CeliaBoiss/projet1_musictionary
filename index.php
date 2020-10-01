4dfef16150dc54aef47b49f320a7d4c330d990ce
LucyMerlier <lucymerlier@gmail.com>
2020-10-01 19:20:37 +0200

    kept filling mainStyle and succeded in pinning footer down at bottom of page

--- index.php
+++ index.php
@@ -1,58 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-<<<<<<< HEAD
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href = "mainStyle.css">
-    <link rel="stylesheet" href="indexStyle.css">
-=======
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-    <link rel = "stylesheet" href = "mainStyle.css">
-    <link rel="stylesheet" href="indexStyle.css">
-    <link rel="stylesheet" href="menuBurger.css">
->>>>>>> 94fa11ed4362807e57e12cded450239b265b3f39
-    <title>MusicTionary</title>
-</head>
-<body>
-    <?php include "header.php"?>
-<<<<<<< HEAD
-    <section title="articles" id="containerArticles">
-=======
-    <main>
-        <section title="articles" id="containerArticles">
->>>>>>> 94fa11ed4362807e57e12cded450239b265b3f39
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
-        <div class="articleBorder">
-            <h2> Thème</h2>
-            <article class="articleBlock">
-                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
-                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
-                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
-            </article>
-        </div>
-        <div class="articleBorder">
-            <h2> Thème</h2>
-            <article class="articleBlock">
-                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
-                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
-                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
-            </article>
-        </div>
-        <div class="articleBorder">
-            <h2> Thème</h2>
-            <article class="articleBlock">
-                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
-                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
-                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
-            </article>
-        </div>
-    </section>
-<<<<<<< HEAD
-=======
-    </main>
->>>>>>> 94fa11ed4362807e57e12cded450239b265b3f39
-    <?php include "footer.php"?>
-    <script src="menuBurger.js"></script>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+        <link rel = "stylesheet" href = "mainStyle.css">
+        <link rel="stylesheet" href="indexStyle.css">
+        <link rel="stylesheet" href="menuBurger.css">
+        <title>MusicTionary</title>
+    </head>
+    <body>
+        <?php include "header.php"?>
+        <main>
+            <section title="articles" id="containerArticles">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+                <div class="articleBorder">
+                    <h2> Thème</h2>
+                    <article class="articleBlock">
+                        <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                        <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                        <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                    </article>
+                </div>
+                <div class="articleBorder">
+                    <h2> Thème</h2>
+                    <article class="articleBlock">
+                        <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                        <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                        <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                    </article>
+                </div>
+                <div class="articleBorder">
+                    <h2> Thème</h2>
+                    <article class="articleBlock">
+                        <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                        <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                        <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                    </article>
+                </div>
+            </section>
+        </main>
+        <?php include "footer.php"?>
+        <script src="menuBurger.js"></script>
+    </body>
 </html>

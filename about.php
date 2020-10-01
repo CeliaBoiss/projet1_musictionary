29ad16616aa1a48b74b0cc0bae135436fd1a24d0
CeliaBoiss <boisseau.celia@gmail.com>
2020-10-01 16:03:53 +0200

    Ajout de la stylesheet du menu burger dans toutes les pages

--- about.php
+++ about.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" href = "mainStyle.css">
+    <link rel="stylesheet" href="menuBurger.css">
     <title>MusicTionary</title>
 </head>
 <body>

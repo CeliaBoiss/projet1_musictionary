876adba6035eb2dec03e829e916e0922bb0b10b9
AshBll <achillebellay@gmail.com>
2020-10-01 10:52:30 +0200

    added div around h2 and article

--- index.php
+++ index.php
@@ -3,31 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="indexStyle.css">
     <title>MusicTionary</title>
 </head>
 <body>
     <?php include "header.php"?>
-    <section title="articles">
+    <section title="articles" id="containerArticles">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
-        <div>
+        <div class="articleBorder">
             <h2> Thème</h2>
-                <article>
-                    <img src="https://via.placeholder.com/75" alt="">
-                    <img src="https://via.placeholder.com/75" alt="">
-                    <img src="https://via.placeholder.com/75" alt="">
-                </article>
-             <h2> Thème</h2>
-                <article>
-                    <img src="https://via.placeholder.com/75" alt="">
-                    <img src="https://via.placeholder.com/75" alt="">
-                    <img src="https://via.placeholder.com/75" alt="">
-                </article>
+            <article class="articleBlock">
+                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+            </article>
+        </div>
+        <div class="articleBorder">
             <h2> Thème</h2>
-                <article>
-                    <img src="https://via.placeholder.com/75" alt="">
-                    <img src="https://via.placeholder.com/75" alt="">
-                    <img src="https://via.placeholder.com/75" alt="">
-                </article>
+            <article class="articleBlock">
+                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+            </article>
+        </div>
+        <div class="articleBorder">
+            <h2> Thème</h2>
+            <article class="articleBlock">
+                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+                <img class="articleImg" src="https://via.placeholder.com/90" alt="">
+            </article>
         </div>
     </section>
     <?php include "footer.php"?>

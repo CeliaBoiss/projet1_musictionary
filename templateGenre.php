a8d36564adf3eb777d29d4ef57c5d5a1fa7cad0d
Yann Raulet <yann.raulet@hotmail.fr>
2020-10-06 11:22:12 +0200

    modify section few exemples

--- templateGenre.php
+++ templateGenre.php
@@ -24,37 +24,34 @@
       </section>
 
       <section title = "Few exemples">
-          <h3>Few Exemples</h3>
-              <ul>
-                  <li><a href = "" class = "chooseVideo" id = "firstVideoToDisplay">video1</a></li>
-                  <li><a href = "" class = "chooseVideo" id = "secondVideoToDisplay">video2</a></li>
-                  <li><a href = "" class = "chooseVideo" id = "thirdVideoToDisplay">video3</a></li>
+          <h1>Few Exemples</h1>
 
-              </ul>
-              <iframe width="350" height="200" id = "displayedVideo" src="https://www.youtube.com/embed/videoseries?list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-      </section>
-
-      <section title = "discoverFirstBlock">
-            <h2>Discover More</h2>
-            <div class="discoverFirstBlock">
-                <img src="https://via.placeholder.com/75" alt="Discover Photo" class = "discoverPicture">
-                <p class = "discoverBlockDisposition">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
-            </div>
+              <iframe width="350" height="200" class = "displayedVideo" src="https://www.youtube.com/embed/videoseries?list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <iframe width="350" height="200" class = "displayedVideo" src="https://www.youtube.com/embed/videoseries?list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <iframe width="350" height="200" class = "displayedVideo" src="https://www.youtube.com/embed/videoseries?list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         </section>
 
-        <section title ="discoverSecondBlock">
-            <div class= "discoverSecondBlock">
-                <p class = "discoverBlockDisposition">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
-                <img src="https://via.placeholder.com/75" alt="Discover Photo" class = "discoverPicture">
-            </div>
-        </section>
+        <section title = "discoverFirstBlock">
+                <h2>Discover More</h2>
+                <div class="discoverFirstBlock">
+                    <img src="https://via.placeholder.com/75" alt="Discover Photo" class = "discoverPicture">
+                    <p class = "discoverBlockDisposition">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+                </div>
+            </section>
 
-        <section title ="discoverThirdBlock">
-            <div class="discoverThirdBlock">
-                <img src="https://via.placeholder.com/75" alt="Discover Photo" class  ="discoverPicture">
-                <p class = "discoverBlockDisposition">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>	
-            </div>
-        </section>
+            <section title ="discoverSecondBlock">
+                <div class= "discoverSecondBlock">
+                    <p class = "discoverBlockDisposition">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+                    <img src="https://via.placeholder.com/75" alt="Discover Photo" class = "discoverPicture">
+                </div>
+            </section>
+
+            <section title ="discoverThirdBlock">
+                <div class="discoverThirdBlock">
+                    <img src="https://via.placeholder.com/75" alt="Discover Photo" class  ="discoverPicture">
+                    <p class = "discoverBlockDisposition">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>	
+                </div>
+            </section>
 
 
     <script src="templateGenreJS.js"></script>

f845f314f81654b02e515cbd596f215dbde1adfb
Yann Raulet <yann.raulet@hotmail.fr>
2020-10-07 16:52:26 +0200

    add disco.js

---
+++ disco.js
@@ -0,0 +1,52 @@
+let displayedVideo = document.querySelector("#displayedVideo");
+
+let firstVideo =  document.querySelector("#firstVideoToDisplay");
+let secondVideo =  document.querySelector("#secondVideoToDisplay");
+let thirdVideo =  document.querySelector("#thirdVideoToDisplay");
+
+let urlFirst = "https://www.youtube.com/embed/sQgd6MccwZc";
+let urlSecond = "https://www.youtube.com/embed/I_izvAbhExY";
+let urlThird = "https://www.youtube.com/embed/CS9OO0S5w2k";
+
+const moreInfo= document.querySelector('#moreInfo');
+
+function showFirstVideo()
+{
+    displayedVideo.src = urlFirst;
+    firstVideo.style.backgroundColor = "rgba(255 , 155 , 0 , 0.40)";
+    secondVideo.style.backgroundColor = "inherit";
+    thirdVideo.style.backgroundColor = "inherit";
+}
+
+function showSecondVideo()
+{
+    displayedVideo.src = urlSecond;
+    firstVideo.style.backgroundColor = "inherit";
+    secondVideo.style.backgroundColor = "rgba(255 , 155 , 0 , 0.40)";
+    thirdVideo.style.backgroundColor = "inherit";
+}
+
+function showThirdVideo()
+{
+    displayedVideo.src = urlThird;
+    firstVideo.style.backgroundColor = "inherit";
+    secondVideo.style.backgroundColor = "inherit";
+    thirdVideo.style.backgroundColor = "rgba(255, 155 , 0 , 0.40)";
+}
+
+function showMoreInfo()
+{
+    const myHiddenParagraph = document.querySelector('.hide');
+
+    if (myHiddenParagraph.style.display === "block") {
+        myHiddenParagraph.style.display = "none";
+    } else {
+        myHiddenParagraph.style.display = "block";
+    }
+}
+
+moreInfo.addEventListener("click", showMoreInfo);
+
+firstVideo.addEventListener("click", showFirstVideo);
+secondVideo.addEventListener("click", showSecondVideo);
+thirdVideo.addEventListener("click", showThirdVideo);

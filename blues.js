7651a987c6b085bb43ea5ea0d8cff9e04432ac1e
LucyMerlier <lucymerlier@gmail.com>
2020-10-07 13:32:50 +0200

    created blues page and started filling in the text

---
+++ blues.js
@@ -0,0 +1,52 @@
+let displayedVideo = document.querySelector("#displayedVideo");
+
+let firstVideo =  document.querySelector("#firstVideoToDisplay");
+let secondVideo =  document.querySelector("#secondVideoToDisplay");
+let thirdVideo =  document.querySelector("#thirdVideoToDisplay");
+
+let urlFirst = "https://www.youtube.com/embed/r4NK7bLnc6I";
+let urlSecond = "https://www.youtube.com/embed/NU5xA6ty0a4";
+let urlThird = "https://www.youtube.com/embed/5Bo3f_9hLkQ";
+
+const moreInfo= document.querySelector('#moreInfo');
+
+function showFirstVideo()
+{
+    displayedVideo.src = urlFirst;
+    firstVideo.style.backgroundColor = "rgba(0 , 0 , 255 , 0.12)";
+    secondVideo.style.backgroundColor = "inherit";
+    thirdVideo.style.backgroundColor = "inherit";
+}
+
+function showSecondVideo()
+{
+    displayedVideo.src = urlSecond;
+    firstVideo.style.backgroundColor = "inherit";
+    secondVideo.style.backgroundColor = "rgba(0 , 0 , 255 , 0.12)";
+    thirdVideo.style.backgroundColor = "inherit";
+}
+
+function showThirdVideo()
+{
+    displayedVideo.src = urlThird;
+    firstVideo.style.backgroundColor = "inherit";
+    secondVideo.style.backgroundColor = "inherit";
+    thirdVideo.style.backgroundColor = "rgba(0 , 0 , 255 , 0.12)";
+}
+
+function showMoreInfo()
+{
+    const myHiddenParagraph = document.querySelector('.hide');
+
+    if (myHiddenParagraph.style.display === "block") {
+        myHiddenParagraph.style.display = "none";
+    } else {
+        myHiddenParagraph.style.display = "block";
+    }
+}
+
+moreInfo.addEventListener("click", showMoreInfo);
+
+firstVideo.addEventListener("click", showFirstVideo);
+secondVideo.addEventListener("click", showSecondVideo);
+thirdVideo.addEventListener("click", showThirdVideo);

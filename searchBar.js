eaf9d2d277638604997a51ae10d7adcd928a6594
CeliaBoiss <boisseau.celia@gmail.com>
2020-10-07 16:12:11 +0200

    Add in searchBar.js the key word for search all pages

--- searchBar.js
+++ searchBar.js
@@ -11,6 +11,24 @@ function findThePage ()
         window.location.replace("about.php");
     } else if (mySearch === "index" || mySearch === "accueil" || mySearch === "home") {
         window.location.replace("index.php");
+    } else if (mySearch === "blues") {
+        window.location.replace("blues.php");
+    } else if (mySearch === "jazz") {
+        window.location.replace("jazz.php");
+    } else if (mySearch === "rap") {
+        window.location.replace("rap.php");
+    } else if (mySearch === "rock") {
+        window.location.replace("rock.php");
+    } else if (mySearch === "country") {
+        window.location.replace("country.php");
+    } else if (mySearch === "ska") {
+        window.location.replace("ska.php");
+    } else if (mySearch === "rnb") {
+        window.location.replace("rnb.php");
+    } else if (mySearch === "electro") {
+        window.location.replace("electro.php");
+    } else if (mySearch === "disco") {
+        window.location.replace("disco.php");
     }
 }
 

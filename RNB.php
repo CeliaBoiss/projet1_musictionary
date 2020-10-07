ed5730c54bde7086bfdf8a777ace904ee3b4f445
Yann Raulet <yann.raulet@hotmail.fr>
2020-10-07 16:51:29 +0200

    add RNB.php

---
+++ RNB.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "mainStyle.css">
+    <link rel="stylesheet" href="menuBurger.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+    <link rel="stylesheet" type="text/css" href="templateGenreStyle.css"/>
+    <link rel="stylesheet" type="text/css" href="templateOrangeStyle.css"/>
+    <title>MusicTionary</title>
+</head>
+<body>
+    <?php include "header.php"?>
+  
+    <main>
+
+        <section title = "Genre">
+            <p><img src="Images/RNB.jpg" alt="Main Title Genre" class="mainTitleGenre"></p>
+            <div class  = "mainTextePresentation">
+                <p>Rhythm and blues, or R&B, is a genre of popular African-American music that emerged in the 1940s.
+                As the name suggests, Rhythm 'n' Blues (“R&B” or “RnB”) is the powerful combination of two old musical genres. 
+                Gospel (the rhythm) on the one hand, which offers a groovy tempo.Blues on the other hand, which delivers warm chords and rich bass.
+                <p>Contemporary RnB gained momentum during the 1990s until it became predominant in the 2000s
+                and combined influences from hip-hop, soul and pop. </p>
+                <p></p>
+                <div id="moreInfo">
+                    <span id="clickInfo">More info ...</span>
+                    <p class="hide">It’s a musical style created above all to make people dance, although there are also many ballads there.
+                    <br> The artists of the moment are: R Kelly, Mary J Blige,Boyz II Men, Destiny’s Child....</p>
+                   
+                </div>
+            </div>
+        </section>
+
+        <section title = "Few exemples">
+            <h3>Few Exemples</h3>
+            <ul>
+                <li class = "genreList"><button class = "chooseVideo" id = "firstVideoToDisplay">Destiny's Child<br>-<br>Say My Name...</button></li>
+                <li class = "genreList"><button class = "chooseVideo" id = "secondVideoToDisplay">R.Kelly<br>-<br>I Believe I Can Fly</button></li>
+                <li class = "genreList"><button class = "chooseVideo" id = "thirdVideoToDisplay">Mary J Blige<br>-<br>Real</button></li>
+            </ul>
+            <div id = "video">
+                <iframe width="350" height="200" id = "displayedVideo" src="https://www.youtube.com/embed/sQgd6MccwZc" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
+        </section>
+                    
+        <h2>Discover More</h2>
+        <section title = "discoverFirstBlock">
+
+                <div class="discoverFirstBlock">
+                    <img src="https://via.placeholder.com/75" alt="Discover Photo" class = "discoverPicture">
+                    <p class = "discoverBlockDisposition">Soul music is a popular African-American music that emerged in the late 1950s in the United States. 
+                        It is considered by some to be a return of rhythm and blues to its roots: gospel (church music). 
+                        <a href="https://www.youtube.com/watch?v=6FOUqQt3Kg0" target="_blank">Go further ...</a></p>
+                </div>
+                    
+
+            </section>
+
+            <hr class="blockSeparator"/>
+
+            <section title ="discoverSecondBlock">
+                <div class= "discoverSecondBlock">
+                    <p class = "discoverBlockDisposition"> Funk that emerged in the mid-1960s in the United States, and that developed in the 1960s and 1970s, 
+                        is characterized by the presence of rhythmic section (guitar, bass, drums), brass or saxophones on rhythmic punctuation (riffs) or solos. 
+                        <a href="https://www.youtube.com/watch?v=uY4cVhXxW64" target="_blank">Go further ...</a></p>
+                    <img src="https://via.placeholder.com/75" alt="Discover Photo" class = "discoverPicture">
+                </div>
+                   
+            </section>
+            
+            <hr class="blockSeparator"/>
+
+            <section title ="discoverThirdBlock">
+                <div class="discoverThirdBlock">
+                    <img src="https://via.placeholder.com/75" alt="Discover Photo" class  ="discoverPicture">
+                    <p class = "discoverBlockDisposition">Hip-hop is a cultural movement that first appeared in New York in the early 1970s. 
+                     Deeply urban, hip-hop brings together graffiti, break-dancing, DJing, beat-boxing and rap.   
+                     The subject of songs is often politics or society.
+                     <a href="https://www.youtube.com/watch?v=PBwAxmrE194" target="_blank">Go further ...</a></p>
+                </div>
+                
+            </section>
+
+    <script src="RNB.js"></script>
+
+    </main>
+
+    <?php include "footer.php"?>
+    <script src="menuBurger.js"></script>
+    <script src="searchBar.js"></script>
+</body>
+</html>

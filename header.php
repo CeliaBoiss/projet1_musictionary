5ca8c8f8e88c1d1b25546a8a6a145c611b29335a
CeliaBoiss <boisseau.celia@gmail.com>
2020-10-07 19:42:29 +0200

    Add few changes to burger menu : colors per music theme, bold when hover and add style on themes

--- header.php
+++ header.php
@@ -10,17 +10,17 @@
             <a href="javascript:void(0)" class="closeButton">&times;</a>
             <ul class = "nav">
                 <li class="themeTitle" id="theme1">Black American Music</li>
-                <li class="navLink"><a class="link" href="blues.php">Blues</a></li>
-                <li class="navLink"><a class="link" href="jazz.php">Jazz</a></li>
-                <li class="navLink"><a class="link" href="rap.php">Rap</a></li>
+                <li class="navLink"><a class="link green" href="blues.php">Blues</a></li>
+                <li class="navLink"><a class="link green" href="jazz.php">Jazz</a></li>
+                <li class="navLink"><a class="link green" href="rap.php">Rap</a></li>
                 <li class="themeTitle" id="theme2">Children of the Blues</li>
-                <li class="navLink"><a class="link" href="rock.php">Rock</a></li>
-                <li class="navLink"><a class="link" href="Country.php">Country</a></li>
-                <li class="navLink"><a class="link" href="ska.php">Ska</a></li>
+                <li class="navLink"><a class="link blue" href="rock.php">Rock</a></li>
+                <li class="navLink"><a class="link blue" href="Country.php">Country</a></li>
+                <li class="navLink"><a class="link blue" href="ska.php">Ska</a></li>
                 <li class="themeTitle" id="theme3">"Pop" Music</li>
-                <li class="navLink"><a class="link" href="rnb.php">RnB</a></li>
-                <li class="navLink"><a class="link" href="electro.php">Electro</a></li>
-                <li class="navLink"><a class="link" href="disco.php">Disco</a></li>
+                <li class="navLink"><a class="link orange" href="rnb.php">RnB</a></li>
+                <li class="navLink"><a class="link orange" href="electro.php">Electro</a></li>
+                <li class="navLink"><a class="link orange" href="disco.php">Disco</a></li>
             </ul>
         </nav>
     </div>

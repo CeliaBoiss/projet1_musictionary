b27e0fcb1e8cb3c6e11e1b7f84d9207973f25452
LucyMerlier <lucymerlier@gmail.com>
2020-10-08 11:06:58 +0200

    added theme descritpions

--- index.php
+++ index.php
@@ -16,12 +16,12 @@
                 <div class="articleBorder" id="themeColor1">
                     <div id="mobile1">
                         <h2 class="mobileTitle">Black American Music</h2>
-                        <p id="hide1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+                        <p id="hide1">The Black Amercian community has generated the foundations for basically every major modern musical genre you can think of!</p>
                     </div>
                     <article class="articleBlock">
                         <div class="descriptive">
                             <h2 class ="desktopTitle">Black American Music</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+                            <p>LThe Black Amercian community has generated the foundations for basically every major modern musical genre you can think of!</p>
                         </div>
                         <img class="articleImg" src="https://via.placeholder.com/90" alt="">
                         <img class="articleImg" src="https://via.placeholder.com/90" alt="">
@@ -32,12 +32,12 @@
                 <div class="articleBorder"  id ="themeColor2">
                     <div id="mobile2">
                         <h2 class="mobileTitle">Children of the Blues</h2>
-                        <p id="hide2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+                        <p id="hide2">Birthed in the USA by the Black community in the 19th century, the Blues is the root of modern music.</p>
                     </div>
                     <article class="articleBlock">
                         <div class="descriptive">
                             <h2 class ="desktopTitle">Children of the Blues</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+                            <p>Birthed in the USA by the Black community in the 19th century, the Blues is the root of modern music.</p>
                         </div>
                         <img class="articleImg" src="https://via.placeholder.com/90" alt="">
                         <img class="articleImg" src="https://via.placeholder.com/90" alt="">
@@ -48,12 +48,12 @@
                 <div class="articleBorder"  id ="themeColor3">
                     <div id="mobile3">
                         <h2 class="mobileTitle">"Pop" Music</h2>
-                        <p id="hide3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+                        <p id="hide3">Not quite a "genre" per say, Pop Music is an umbrella term for everything and anything that's playing on the radio or at the top of the charts.</p>
                     </div>
                     <article class="articleBlock">
                         <div class="descriptive">
                             <h2 class ="desktopTitle">"Pop" Music</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eget felis in ante consequat dignissim.</p>
+                            <p>Not quite a "genre" per say, Pop Music is an umbrella term for everything and anything that's playing on the radio or at the top of the charts.</p>
                         </div>
                         <img class="articleImg" src="https://via.placeholder.com/90" alt="">
                         <img class="articleImg" src="https://via.placeholder.com/90" alt="">

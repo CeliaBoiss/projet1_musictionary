4add28698433f9f62242ac89a235442d2bf5494f
Yann Raulet <yann.raulet@hotmail.fr>
2020-10-08 20:32:55 +0200

    modify main section

--- jazz.php
+++ jazz.php
@@ -27,40 +27,48 @@
             </div>
         </section>
 
-        <section title = "Few exemples">
-            <h3>A few Examples</h3>
-            <ul>
-                <li class = "genreList"><button class = "chooseVideo" id = "firstVideoToDisplay">Duke Ellington with Ella Fitzgerald<br>-<br>Caravan</button></li>
-                <li class = "genreList"><button class = "chooseVideo" id = "secondVideoToDisplay">Miles Davis<br>-<br>Tutu</button></li>
-                <li class = "genreList"><button class = "chooseVideo" id = "thirdVideoToDisplay">Snarky Puppy<br>-<br>Bad Kids to the Back</button></li>
-            </ul>
+        <section title = "Few examples">
+        <h3 class="fewExample">A few Examples</h3>
+        <hr class="blockSeparator"/> 
+            <div class="containExamples">
+                <ul>
+                    <li class = "genreList"><button class = "chooseVideo" id = "firstVideoToDisplay">Duke Ellington with Ella Fitzgerald<br>-<br>Caravan</button></li>
+                    <li class = "genreList"><button class = "chooseVideo" id = "secondVideoToDisplay">Miles Davis<br>-<br>Tutu</button></li>
+                    <li class = "genreList"><button class = "chooseVideo" id = "thirdVideoToDisplay">Snarky Puppy<br>-<br>Bad Kids to the Back</button></li>
+                </ul>
+            </div>
             <div id = "video">
                 <iframe width="350" height="200" id = "displayedVideo" src="https://www.youtube.com/embed/X3ADroVuFwM" allow="autoplay; encrypted-media" allowfullscreen></iframe>
             </div>
         </section>
-
+        <h2>Discover More</h2>
+        <hr class="blockSeparator"/> 
         <section title = "discoverFirstBlock">
-                <h2>Discover More</h2>
-                <div class="discoverFirstBlock">
-                    <img src="Images/jazzBebop.jpg" alt="Discover Photo" class = "discoverPicture">
-                    <p class = "discoverBlockDisposition">Bebop - This style, which leaves room for interpretation, is characterized by an often very fast tempo, dynamic phrasing and by the fact that it is not necessarily linked to the dance.
-                     <a href="https://www.youtube.com/watch?v=okZYCS_TCO8&feature=emb_logo"  target="_blank">Go further ...</a></p>
-                </div>
-            </section>
-
-            <section title ="discoverSecondBlock">
-                <div class= "discoverSecondBlock">
-                    <p class = "discoverBlockDisposition">Free jazz - An experimental approach to jazz improvisation that developed in the late 1950s and early 1960s when musicians attempted to change or break down jazz conventions. <a href="https://www.youtube.com/watch?v=d0HB8ybKJzo" target="_blank">Go further ...</a></p>
-                    <img src="Images/jazzFree.jpg" alt="Discover Photo" class = "discoverPicture">
-                </div>
-            </section>
-
-            <section title ="discoverThirdBlock">
-                <div class="discoverThirdBlock">
-                    <img src="Images/jazzFusion.jpg" alt="Discover Photo" class  ="discoverPicture">
-                    <p class = "discoverBlockDisposition">Jazz fusion - A music genre that developed in the late 1960s when musicians combined jazz harmony and improvisation with rock music, funk, and rhythm and blues. <a href="https://www.youtube.com/watch?v=R0oY_47pwJ8" target="_blank">Go further ...</a></p>	
-                </div>
-            </section>
+            <div class="discoverFirstBlock">
+                <img src="Images/jazzBebop.jpg" alt="Discover Photo" class = "discoverPicture">
+                <p class = "discoverBlockDisposition"><span class="subGenreTitle">BEBOP : </span><br><br>This style, which leaves room for interpretation, is characterized by an often very fast tempo, dynamic phrasing and by the fact that it is not necessarily linked to the dance.
+                <a href="https://www.youtube.com/watch?v=okZYCS_TCO8&feature=emb_logo" class="hideGoFurther" target="_blank">Go further ...</a></p>
+                <iframe id="hideVideo" width="0" height="0" src="https://www.youtube.com/embed/okZYCS_TCO8" frameborder="0" allow="autoplay; encrypted-med" allowfullscreen></iframe>
+            </div>
+        </section>
+        <hr class="blockSeparator"/> 
+        <section title ="discoverSecondBlock">
+            <div class= "discoverSecondBlock">
+                <iframe id="hideVideo" width="0" height="0" src="https://www.youtube.com/embed/d0HB8ybKJzo" frameborder="0" allow="autoplay; encrypted-med" allowfullscreen></iframe>
+                <p class = "discoverBlockDisposition"><span class="subGenreTitle">FREE JAZZ :</span><br><br>An experimental approach to jazz improvisation that developed in the late 1950s and early 1960s when musicians attempted to change or break down jazz conventions. 
+                <a href="https://www.youtube.com/watch?v=d0HB8ybKJzo" class="hideGoFurther" target="_blank">Go further ...</a></p>
+                <img src="Images/jazzFree.jpg" alt="Discover Photo" class = "discoverPicture">
+            </div>
+        </section>
+        <hr class="blockSeparator"/> 
+        <section title ="discoverThirdBlock">
+            <div class="discoverThirdBlock">
+                <img src="Images/jazzFusion.jpg" alt="Discover Photo" class  ="discoverPicture">
+                <p class = "discoverBlockDisposition"><span class="subGenreTitle">JAZZ FUSION :</span><br><br>A music genre that developed in the late 1960s when musicians combined jazz harmony and improvisation with rock music, funk, and rhythm and blues. 
+                <a href="https://www.youtube.com/watch?v=R0oY_47pwJ8" class="hideGoFurther" target="_blank">Go further ...</a></p>	
+                <iframe id="hideVideo" width="0" height="0" src="https://www.youtube.com/embed/R0oY_47pwJ8"frameborder="0" allow="autoplay; encrypted-med" allowfullscreen></iframe>
+            </div>
+        </section>
 
 
     <script src="jazz.js"></script>

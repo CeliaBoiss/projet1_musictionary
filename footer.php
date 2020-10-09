f09989e9c0285e7ae6633c6ecc456ce1e6c5598b
LucyMerlier <lucymerlier@gmail.com>
2020-10-09 11:37:58 +0200

    added legal mentions link in footer

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
         <ul class = "footerList">
             <li class = "footerLink"><a href="about.php">About Us</a></li>
             <li class = "footerLink"><a href="contactus.php">Contact Us</a></li>
+            <li class = "footerLink"><a href="legalMentions.php">Legal Mentions</a></li>
         </ul>
     </div>
 </footer>

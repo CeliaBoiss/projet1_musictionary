a416fa4ee44e103f5e18291a6059b4d7a5155850
LucyMerlier <lucymerlier@gmail.com>
2020-10-09 11:22:03 +0200

    added banners for desktop index with js

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ let myTheme1 = document.querySelector('#mobile1');
 let myTheme2 = document.querySelector('#mobile2');
 let myTheme3 = document.querySelector('#mobile3');
 
+let bluesImage = document.querySelector('#bluesImage');
+let jazzImage = document.querySelector('#jazzImage');
+let rapImage = document.querySelector('#rapImage');
+let rockImage = document.querySelector('#rockImage');
+let countryImage = document.querySelector('#countryImage');
+let skaImage = document.querySelector('#skaImage');
+let rnbImage = document.querySelector('#rnbImage');
+let electroImage = document.querySelector('#electroImage');
+let discoImage = document.querySelector('#discoImage');
+
+// code to change image link in index so that it's of appropriate size in desktop version
+
+if (window.matchMedia("(min-width: 812px)").matches) {
+    bluesImage.src = "Images/bluesBox.jpg";
+    jazzImage.src = "Images/jazzBox.jpg";
+    rapImage.src = "Images/rapBox.jpg";
+    rockImage.src = "Images/rockBox.jpg";
+    countryImage.src = "Images/countryBox.jpg";
+    skaImage.src = "Images/skaBox.jpg";
+    rnbImage.src = "Images/rnbBox.jpg";
+    electroImage.src = "Images/electroBox.jpg";
+    discoImage.src = "Images/discoBox.png";
+}
+
 function showOrHide(event)
 {
     const myHiddenParagraph = event.target.nextElementSibling;

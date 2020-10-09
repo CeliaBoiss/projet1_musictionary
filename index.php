cb5a35c6a2fe4ae5fc55cbdf0fc33fc922318c34
CeliaBoiss <boisseau.celia@gmail.com>
2020-10-09 11:01:50 +0200

    Add an arrow beside the theme names in index.php for showing user he can click on it

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
                 <p id="desktopBorder">Welcome to MusicTionary, your online music genre dictionary! <br><br>"A music genre is a conventional category that identifies some pieces of music as belonging to a shared tradition or set of conventions."</p>
                 <div class="articleBorder" id="themeColor1">
                     <div id="mobile1">
-                        <h2 class="mobileTitle">Black American Music</h2>
+                        <h2 class="mobileTitle">Black American Music ⬇</h2>
                         <p id="hide1">The Black Amercian community has generated the foundations for basically every major modern musical genre you can think of!</p>
                     </div>
                     <article class="articleBlock">
@@ -31,7 +31,7 @@
 
                 <div class="articleBorder"  id ="themeColor2">
                     <div id="mobile2">
-                        <h2 class="mobileTitle">Children of the Blues</h2>
+                        <h2 class="mobileTitle">Children of the Blues ⬇</h2>
                         <p id="hide2">Birthed in the USA by the Black community in the 19th century, the Blues is the root of modern music.</p>
                     </div>
                     <article class="articleBlock">
@@ -47,7 +47,7 @@
 
                 <div class="articleBorder"  id ="themeColor3">
                     <div id="mobile3">
-                        <h2 class="mobileTitle">"Pop" Music</h2>
+                        <h2 class="mobileTitle">"Pop" Music ⬇</h2>
                         <p id="hide3">Not quite a "genre" per say, Pop Music is an umbrella term for everything and anything that's playing on the radio or at the top of the charts.</p>
                     </div>
                     <article class="articleBlock">

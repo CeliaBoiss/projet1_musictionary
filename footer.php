440419cbac08f979031d9fe05175941b960b614d
CeliaBoiss <boisseau.celia@gmail.com>
2020-10-10 17:49:16 +0200

    Remove the text decoration underline on the link of footer

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
     <button id="returnTop">Top</button>
     <div id = "footerFlexDiv">
         <ul class = "footerList">
-            <li class = "footerLink"><a href="about.php">About Us</a></li>
-            <li class = "footerLink"><a href="contactus.php">Contact Us</a></li>
-            <li class = "footerLink"><a href="legalMentions.php">Legal Mentions</a></li>
+            <li class = "footerLink"><a class="aFooter" href="about.php">About Us</a></li>
+            <li class = "footerLink"><a class="aFooter" href="contactus.php">Contact Us</a></li>
+            <li class = "footerLink"><a class="aFooter" href="legalMentions.php">Legal Mentions</a></li>
         </ul>
     </div>
 </footer>
